d039da691eb78331f29dbb659a3bec83f6545681
flexi767 <flexi767@gmail.com>
2026-03-28 17:40:26 +0100

    feat: add basePath prop to RangeFilter and PriceChangeFilter

--- components/PriceChangeFilter.tsx
+++ components/PriceChangeFilter.tsx
@@ -2,10 +2,10 @@
 
 import RangeFilter from './RangeFilter';
 
-interface Props { min: number; max: number; }
+interface Props { min: number; max: number; basePath?: string; }
 
 const fmt = (v: number) => v > 0 ? `+${v}` : String(v);
 
-export default function PriceChangeFilter({ min, max }: Props) {
-  return <RangeFilter min={min} max={max} paramLow="pc_min" paramHigh="pc_max" label="Δ" fmt={fmt} />;
+export default function PriceChangeFilter({ min, max, basePath }: Props) {
+  return <RangeFilter min={min} max={max} paramLow="pc_min" paramHigh="pc_max" label="Δ" fmt={fmt} basePath={basePath} />;
 }
